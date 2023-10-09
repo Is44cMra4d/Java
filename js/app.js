555b3b72d324c674877e258f8f614e0373a931a1
Is44cMra4d <145473135+Is44cMra4d@users.noreply.github.com>
2023-10-09 00:13:37 -0300

    Add files via upload
    lo hice en consol log porque con alert me saltaba error no se porque

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+cuantas letras tiene t nombre
+
+ const nombre = prompt("Ingresa tu Nombre")
+console.log(nombre.length);
+
+
+
+ Cual es tu edad y lo multiplico 100
+
+ const edad = prompt("¿cual es tu edad?")
+
+ console.log(edad * 100);
+
+
+
+ Dime tu nombre y lo divido letra por letra 
+
+
+const nombre = prompt("Ingresa tu Nombre")
+
+ console.log(nombre.split(""));
